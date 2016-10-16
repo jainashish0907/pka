37350f9f37c0f96d42c962980318b5b2572ba13a
jainashish0907 <jainashish0907@gmail.com>
2016-10-16 18:14:38 +0530

    first commit

---
+++ app/src/main/java/jain/pathshala/kholo/abhiyaan/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package jain.pathshala.kholo.abhiyaan.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import jain.pathshala.kholo.abhiyaan.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/jain/pathshala/kholo/abhiyaan/util/XMLParser.java
@@ -0,0 +1,7 @@
+package jain.pathshala.kholo.abhiyaan.util;
+
+/**
+ * Created by RITU on 16-10-2016.
+ */
+public class XMLParser {
+}
